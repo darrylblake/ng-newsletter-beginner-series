3f757c88dc3e8301fe686a49a53ca6ac404a2c8b
Darryl Blake <darryl@nikordi.com>
2015-08-01 17:13:56 -0700

    Added $http AJAX request to NPR. Very basically display data.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var app = angular.module('myApp', []);
+var apiKey = 'MDIwMDQwNzgyMDE0Mzg0NzM4OTMwYzkzNw001';
+var nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
 
-app.controller('PlayerController', ['$scope', function($scope) {
+
+app.controller('PlayerController', function($scope, $http) {
   $scope.playing = false;
   $scope.audio = document.createElement('audio');
   $scope.audio.src = 'http://www.stephaniequinn.com/Music/Hungarian%20Dance.mp3';
@@ -17,7 +20,39 @@ app.controller('PlayerController', ['$scope', function($scope) {
       $scope.stop()
     });
   });
-}]);
+
+  $http({
+    method: 'JSONP',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+  }).success(function(data, status) {
+    $scope.programs = data.list.story;
+    // Now we have a list of the stories (data.list.story)
+    // in the data object that the NPR API 
+    // returns in JSON that looks like:
+    // data: { "list": {
+    //   "title": ...
+    //   "story": [
+    //     { "id": ...
+    //       "title": ...
+  }).error(function(data, status) {
+    // Some error occurred
+  });
+
+});
 
 app.controller('RelatedController', ['$scope', function($scope) {
 }])
+
+
+app.controller('MyController', function($scope) {
+  $scope.person = { name: "Ari Lerner" };
+  var updateClock = function() {
+    $scope.clock = new Date();
+  };
+  $timeout(function updateTime() {
+    $scope.$apply(updateClock);
+    $timeout(updateTime(), 1000);
+  }, 1000);
+
+  updateClock();
+});
